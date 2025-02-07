72c61a6072e8fd5ad55e8152523daf05e4de10d1
unknown <jibon602947@gmail.com>
2025-02-07 11:40:54 +0600

    prisma to DB

--- components/header.jsx
+++ components/header.jsx
@@ -17,9 +17,12 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import Image from "next/image";
+import { checkUser } from "@/lib/checkUser";
 // import { checkUser } from "@/lib/checkUser";
 
-const Header = () => {
+const Header = async () => {
+  await checkUser();
+
   return (
     <header className="fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-50">
       <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
